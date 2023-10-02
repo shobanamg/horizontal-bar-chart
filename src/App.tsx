c51ebcb3c52fe79957c0808cc23d53c5cc324118
Shobana Mathiarul <mathiarulshobana@gmail.com>
2023-10-02 14:26:35 +0200

    feat: Added example chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    indexAxis: 'y' as const,
+    elements: {
+        bar: {
+            borderWidth: 2,
+        },
+    },
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'right' as const,
+        },
+        title: {
+            display: true,
+            text: 'spend per category level 1',
+        },
+    },
+};
+
+
+const data2 = {
+     labels : [
+        'Uncategorised',
+        'Purchased Goods',
+        'Capital Goods',
+        'Fuel- and Energy-Related Activities (Not Included in Scope 1 or Scope 2)',
+        'Upstream Transportation and Distribution',
+        'Waste Generated in Operations',
+        'Business Travel',
+        'Employee Commuting',
+        'Upstream Leased Assets',
+    ],
+    datasets: [
+        {
+            label: 'Dataset 1',
+            data: [7000,1000,2000,8000,9000,3000,4000,5000,6000],
+            borderColor: 'rgb(11, 63, 150)',
+            backgroundColor: 'rgba(82, 122, 191)',
+            borderWidth: 1,
+        },
+        {
+            label: 'Dataset 2',
+            data: [1500,3300,7500,2000,1000,5500,9000,2200,6000],
+            borderColor: 'rgb(217, 108, 20)',
+            backgroundColor: 'rgba(217, 135, 69)',
+            borderWidth: 1,
+        },
+    ],
+
+
 }
 
-export default App;
+
+export function App() {
+    return <div>
+        <h1>ChartJS</h1>
+        <h2>spend per category level 1</h2>
+        <div style={{padding: '20px',       width: '50%'}}>
+        <Bar options={options} data={data2} />
+        </div>
+        </div>;
+}
